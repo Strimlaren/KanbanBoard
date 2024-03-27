9a43ff645585c3b50d7bd5d711f448df213ad8f0
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2024-03-28 00:38:32 +0100

    moved cards state up to ColumnList component, columns are now made dynamically from carddata

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,26 +1,20 @@
 import React from "react";
-import { useState } from "react";
 import TodoCard from "./TodoCard";
-import { carddata } from "../assets/carddata.jsx";
 
-export default function Column({ title }) {
-  const [cards, setCards] = useState(carddata);
-
-  const columnCards = cards.filter((card) => card.columnTitle === title);
-
+export default function Column({ card }) {
   return (
     <>
       <div className="column">
-        <div className="column-title">{columnCards[0].columnTitle}</div>
+        <div className="column-title">{card.columnTitle}</div>
         <div className="cards-container">
-          {columnCards[0].cards.map((card, index) => {
+          {card.cards.map((cardx, index) => {
             return (
               <TodoCard
                 key={index}
-                title={card.cardTitle}
-                content={card.content}
-                creator={card.creator}
-                date={card.date}
+                title={cardx.cardTitle}
+                content={cardx.content}
+                creator={cardx.creator}
+                date={cardx.date}
               />
             );
           })}
--- src/components/ColumnList.jsx
+++ src/components/ColumnList.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Column from "./Column";
+import { useState } from "react";
+import { carddata } from "../assets/carddata.jsx";
 
 export default function ColumnList() {
+  const [cards, setCards] = useState(carddata);
+
   return (
     <>
       <div className="column-list">
-        <Column title={"To Do"} />
-        <Column title={"Doing"} />
-        <Column title={"Done"} />
+        {cards.map((card, index) => (
+          <Column card={card} key={index} />
+        ))}
       </div>
     </>
   );
