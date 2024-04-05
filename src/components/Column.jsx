637df4875e958512398bd4089862d3e8ad12c564
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2024-04-05 17:16:31 +0200

    Added column buttons for horisontal column movement and column title editing

--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -3,7 +3,12 @@ import { ModalContext } from "./Provider.jsx";
 import TodoCard from "./TodoCard";
 import NewModal from "./NewModal.jsx";
 import EditModal from "./EditModal.jsx";
-import { AddNewIcon } from "../assets/images/icons.jsx";
+import {
+  AddNewIcon,
+  ArrowRight,
+  ArrowLeft,
+  EditTitle,
+} from "../assets/images/icons.jsx";
 
 export default function Column({ card, colpos, nav }) {
   const [
@@ -19,12 +24,24 @@ export default function Column({ card, colpos, nav }) {
       {isEditModalOpen[0] && <EditModal />}
       <div className="column">
         <div className="column-title">
-          <h2
-            className={nav ? "column-title-link" : undefined}
-            onClick={nav ? () => nav(`/col/${card.columnTitle}`) : undefined}>
-            {card.columnTitle}
-          </h2>
-          <span className="path">{`/col/${card.columnTitle}`}</span>
+          <span className="column-edit-button">
+            <ArrowLeft />
+          </span>
+          <div>
+            <h2
+              className={nav ? "column-title-link" : undefined}
+              onClick={nav ? () => nav(`/col/${card.columnTitle}`) : undefined}>
+              {card.columnTitle}
+            </h2>
+            <span className="path">{`/col/${card.columnTitle}`}</span>
+          </div>
+          <span className="column-edit-button">
+            <ArrowRight />
+          </span>
+          <span className="column-edit-button edit-title-icon">
+            <EditTitle />
+          </span>
+
           {colpos === 0 && (
             <div className="add-icon">
               <span onClick={handleToggleNewModal}>
@@ -33,6 +50,7 @@ export default function Column({ card, colpos, nav }) {
             </div>
           )}
         </div>
+
         <div className="cards-container">
           {card.cards.map((cardx, index) => {
             return (
