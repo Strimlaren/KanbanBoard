b4309dec8a7aefc328c883a4a70702e32e8f6035
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2024-04-05 11:41:09 +0200

    New error message

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -13,7 +13,8 @@ export default function Error() {
       <section className="column-list">
         <div className="column">
           <div className="column-title">
-            <h2>Cake? Sushi?</h2>
+            <h2>No spoon?</h2>
+            <span className="path">/matrix/loophole</span>
           </div>
 
           <div className="cards-container">
@@ -26,19 +27,19 @@ export default function Error() {
                   <ArrowDown />
                 </span>
               </div>
-              <h3>404 The cake is a lie</h3>
+              <h3>404 There is no spoon</h3>
               <div className="p-content-div">
                 <p>
-                  So is the cookie. Sushi is real tho. I dont know why this is
-                  relevant, it sure as hell wont help you find what you were
-                  looking for.
+                  It is just a reflection of yourself that is bending. I dont
+                  know why this is relevant, it sure as hell wont help you find
+                  what you were looking for.
                 </p>
               </div>
               <div className="card-footer">
                 <p>
                   <span>Added </span>
                   2012-12-12 by
-                  <span className="highlight"> Mom</span>
+                  <span className="highlight"> Neo</span>
                 </p>
                 <div className="icon-container">
                   <span className="left-icon">
