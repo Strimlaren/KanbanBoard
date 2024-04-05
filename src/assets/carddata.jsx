471182271dd09d394cdfb919a15e1733a86711dd
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2024-04-05 11:03:53 +0200

    Added more base instruction cards

--- src/assets/carddata.jsx
+++ src/assets/carddata.jsx
@@ -10,6 +10,14 @@ export let carddata = [
         creator: "Strimlaren",
         edited: false,
       },
+      {
+        cardTitle: "Routing",
+        content:
+          "You can select and browse to each column separately by clicking its title. Hovering will display the relative path in case you came from the stone-age and wish to type it yourself in the browser searchbar.",
+        date: "1965-02-12",
+        creator: "MMC",
+        edited: false,
+      },
     ],
   },
   {
@@ -29,6 +37,22 @@ export let carddata = [
           "Move back todos with the back button, delete notes with the trash button, or move them forward by the checkbutton.",
         date: "2019-05-08",
         creator: "Strimlaren",
+        edited: false,
+      },
+      {
+        cardTitle: "Vertical sorting",
+        content:
+          "Conditionally rendered arrows placed in top right corner for moving cards vertically inside each column. Will resize for easier targeting.",
+        date: "1997-10-02",
+        creator: "Mirza",
+        edited: false,
+      },
+      {
+        cardTitle: "Edited/Added ?",
+        content:
+          "Ground-breaking functionality lets you keep track of which cards were edited and when, with color-coding and smooooooth transitions.",
+        date: "1958-04-23",
+        creator: "Mirza",
         edited: false,
       },
     ],
