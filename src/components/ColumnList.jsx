bc1a0560eb542773822c6a9b02a9992c92f30cae
Mirza Mesinovic <mirza.mesinovic@chasacademy.se>
2024-04-09 19:53:12 +0200

    Fixed routing crash issue. Needed to put dragdropcontext in provider.jsx to make it available even to routed pages like routedcolumnlist.jsx

--- src/components/ColumnList.jsx
+++ src/components/ColumnList.jsx
@@ -2,7 +2,6 @@ import Column from "./Column";
 import { useContext } from "react";
 import { DataContext } from "./Provider";
 import { useNavigate } from "react-router";
-import { DragDropContext } from "@hello-pangea/dnd";
 
 /* Creates all Columns in base mode (when no routing is used) */
 export default function ColumnList() {
@@ -10,62 +9,19 @@ export default function ColumnList() {
   const nav = useNavigate();
   // const length = cards.length;
 
-  /* Handles the moving of items when releasing drag */
-  function onDragEnd(result) {
-    const { destination, source, draggableId } = result;
-    if (!destination) return;
-    if (
-      destination.droppableId === source.droppableId &&
-      destination.index === source.index
-    )
-      return;
-
-    setCards((prevCards) => {
-      const newCards = prevCards.map((column) => ({
-        ...column,
-        cards: column.cards.map((card) => ({ ...card })),
-      }));
-      /* If user tries to move a card to a spot that is not droppable, or back to the same spot, do nothing */
-      /* Remove the dragged card from its source container */
-      const sourceCards = newCards[source.droppableId].cards;
-      sourceCards.splice(source.index, 1);
-      /* Get hold of the card that is being dragged */
-      const movedItem = cards[source.droppableId].cards.filter(
-        (item) =>
-          !sourceCards.some(
-            (sourceItem) => JSON.stringify(sourceItem) === JSON.stringify(item)
-          )
-      );
-      /* Add the dragged item to the destination column */
-
-      console.log(destination.droppableId);
-      newCards[destination.droppableId].cards.splice(
-        destination.index,
-        0,
-        movedItem[0]
-      );
-
-      /* Set the source column to same content but without the dragged card */
-      newCards[source.droppableId].cards = sourceCards;
-      return newCards;
-    });
-  }
-
   return (
     <>
       <section className="column-list">
-        <DragDropContext onDragEnd={onDragEnd}>
-          {cards.map((card, index) => (
-            <Column
-              key={index}
-              card={card}
-              colpos={index}
-              nav={nav}
-              length={length}
-              routed={false}
-            />
-          ))}
-        </DragDropContext>
+        {cards.map((card, index) => (
+          <Column
+            key={index}
+            card={card}
+            colpos={index}
+            nav={nav}
+            length={length}
+            routed={false}
+          />
+        ))}
       </section>
     </>
   );
--- src/components/Provider.jsx
+++ src/components/Provider.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState, useEffect } from "react";
 import { carddata } from "../assets/carddata.jsx";
+import { DragDropContext } from "@hello-pangea/dnd";
 
 export const ModalContext = createContext();
 export const DataContext = createContext();
@@ -28,18 +29,61 @@ export default function Provider({ children }) {
     localStorage.setItem("data", JSON.stringify(cards));
   }, [cards]);
 
+  /* Handles the moving of items when releasing drag */
+  function onDragEnd(result) {
+    const { destination, source, draggableId } = result;
+    if (!destination) return;
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    )
+      return;
+
+    setCards((prevCards) => {
+      const newCards = prevCards.map((column) => ({
+        ...column,
+        cards: column.cards.map((card) => ({ ...card })),
+      }));
+      /* If user tries to move a card to a spot that is not droppable, or back to the same spot, do nothing */
+      /* Remove the dragged card from its source container */
+      const sourceCards = newCards[source.droppableId].cards;
+      sourceCards.splice(source.index, 1);
+      /* Get hold of the card that is being dragged */
+      const movedItem = cards[source.droppableId].cards.filter(
+        (item) =>
+          !sourceCards.some(
+            (sourceItem) => JSON.stringify(sourceItem) === JSON.stringify(item)
+          )
+      );
+      /* Add the dragged item to the destination column */
+
+      console.log(destination.droppableId);
+      newCards[destination.droppableId].cards.splice(
+        destination.index,
+        0,
+        movedItem[0]
+      );
+
+      /* Set the source column to same content but without the dragged card */
+      newCards[source.droppableId].cards = sourceCards;
+      return newCards;
+    });
+  }
+
   return (
-    <ModalContext.Provider
-      value={[
-        isNewModalOpen,
-        handleToggleNewModal,
-        isEditModalOpen,
-        handleToggleEditModal,
-        setIsEditModalOpen,
-      ]}>
-      <DataContext.Provider value={[cards, setCards]}>
-        {children}
-      </DataContext.Provider>
-    </ModalContext.Provider>
+    <DragDropContext onDragEnd={onDragEnd}>
+      <ModalContext.Provider
+        value={[
+          isNewModalOpen,
+          handleToggleNewModal,
+          isEditModalOpen,
+          handleToggleEditModal,
+          setIsEditModalOpen,
+        ]}>
+        <DataContext.Provider value={[cards, setCards]}>
+          {children}
+        </DataContext.Provider>
+      </ModalContext.Provider>
+    </DragDropContext>
   );
 }
